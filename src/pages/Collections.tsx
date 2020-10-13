e4db5814bcacff30d2e0cbaba85a033209b3795b
Ivan Cheng <ivan@ivancheng.co.uk>
2020-10-13 10:42:06 +0100

    basic layout

--- src/pages/Collections.tsx
+++ src/pages/Collections.tsx
@@ -3,18 +3,26 @@ import React, { Component } from "react";
 import { myStyles } from "../utils/myStyles";
 import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { collectionInitialState } from "../store/initialStates/collectionInitialState";
 // import { createStackNavigator } from "@react-navigation/stack";
 
 const Stack = createStackNavigator();
 
-const Collections = ({navigation}:any) => {
+const Collections = ({ navigation }: any) => {
+  const collections = collectionInitialState;
   return (
     <SafeAreaView style={myStyles.container}>
       <Text>Spaceholder for Searchbar</Text>
       <Text>Collections here</Text>
-      <TouchableOpacity onPress={() => navigation.navigate("Items")}>
-        <Text>Click to go to items page</Text>
-      </TouchableOpacity>
+      {collections &&
+        collections.map((c) => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Items")}
+            key={c.name}
+          >
+            <Text>{c.name}</Text>
+          </TouchableOpacity>
+        ))}
     </SafeAreaView>
   );
 };
---
+++ src/store/initialStates/itemsInitialState.ts
@@ -0,0 +1,29 @@
+export const itemInitialState = [
+  {
+    name: "Turtle",
+    description: "A lovely turtle",
+    type: "Pins",
+    // dateCreated: 2,
+    picture: "",
+    city: "London",
+    id: ""
+  },
+  {
+    name: "Hard Rock Amsterdam",
+    description: "Guitar",
+    type: "Pins",
+    // dateCreated: 2,
+    picture: "",
+    city: "Amsterdam",
+    id: ""
+  },
+  {
+    name: "A250e",
+    description: "Hybrid Mercedes-Benz",
+    type: "Cars",
+    // dateCreated: 2,
+    picture: "",
+    city: "London",
+    id: ""
+  },
+]
