107b2516d1a5a859ec13ebae3ab2578af70a73fc
Ivan Cheng <ivan@ivancheng.co.uk>
2020-11-19 10:34:47 +0000

    removed init states and added splash image

--- src/store/initialStates/collectionInitialState.ts
+++ src/store/initialStates/collectionInitialState.ts
@@ -1,18 +1,1 @@
-export const collectionInitialState = [
-  {
-    name: "Pins",
-    image: "",
-    id: "_95scyqx24rb",
-    dateCreated: new Date("2019-06-28").getTime(),
-    dateModified: new Date("2019-06-28").getTime(),
-    orientation: "landscape",
-  },
-  {
-    name: "Cars",
-    image: "",
-    id: "_pw4j2ehhc3s",
-    dateCreated: new Date("2018-06-28").getTime(),
-    dateModified: new Date("2018-06-28").getTime(),
-    orientation: "landscape",
-  },
-];
+export const collectionInitialState = [];
